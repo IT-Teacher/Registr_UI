a8e0145f6e928a456d9c0051d8c7a38b4013577a
IT-Teacher <mirgiyozovm@gmail.com>
2022-06-13 15:54:19 +0500

    Regisrt_UI

---
+++ app/src/main/java/uz/it_teacher/convertor/MainActivity.java
@@ -0,0 +1,14 @@
+package uz.it_teacher.convertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
